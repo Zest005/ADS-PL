d33465892b26e2624b1a6b6e1890a5503181fb97
Zest005 <81220254+Zest005@users.noreply.github.com>
2021-06-13 23:33:09 +0300

    PL3_done

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            List list = new List();
+            
+        }
+    }
+}
